2cd03dbfe0a431c485a8da9f4a2ccb60773c7c90
shashank <shashankattrey26@gmail.com>
2025-12-31 11:56:35 +0530

    Initial FastAPI credit card API

--- netlify/functions/get-user.js
+++ netlify/functions/get-user.js
@@ -1,27 +1,15 @@
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
 exports.handler = async (event) => {
-  console.log("GET USER CALLED", event.queryStringParameters);
-
   const user_id = parseInt(event.queryStringParameters?.id || "1");
 
   return {
     statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    },
+    headers: { "Access-Control-Allow-Origin": "*" },
     body: JSON.stringify({
       user_id,
       phone: "+919876543210",
       full_name: "Shashank Attrey",
       pincode: "302001",
-      status: "mock_success",
+      status: "live_success",
     }),
   };
 };
